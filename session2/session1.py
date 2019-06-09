9ef557684d8ffc23daeb4d944b0cb71013a46230
đoàn Tuấn <tuan14112003@gmail.com>
2019-06-09 12:04:58 +0700

    .

---
+++ session2/session1.py
@@ -0,0 +1,9 @@
+print("chuong trinh tinh dien tich tam giac")
+a=float(input("nhap gia tri canh day"))
+b=float(input("nhap gia tri do cao"))
+s=(a*b)/2
+print(s)
+print("-----------------------")
+print("chuong trinh tinh dien tich hinh tron")
+a=int(input("nhap gia tri ban kinh"))
+print("dien tich hinh tron la",a*a*3.14)
